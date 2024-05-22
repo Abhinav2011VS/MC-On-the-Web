36a25f27c7061ed3c601a8661213ea32668aa29d
Abhinav VS <abhinav2011vs@gmail.com>
2024-05-22 22:15:29 +0530

    UPDATE TEST

--- testing-1/main.py
+++ testing-1/main.py
@@ -1,14 +1,14 @@
 # main.py
 
+import numpy as np
 import pyglet
 import nbtlib
 import base36
 import pyglm
 
 # Example usage of the imported libraries
+print("NumPy version:", np.__version__)
 print("Pyglet version:", pyglet.version)
 print("Base36 encoded 12345:", base36.dumps(12345))
 vec = pyglm.vec3(1, 2, 3)
 print("Created a 3D vector using pyglm:", vec)
-
-# Your additional Python code here
